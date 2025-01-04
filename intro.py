111c054d6928db65ef7d05385fc802efc0aae178
suraj-sharma3 <suraj.sharma13395@gmail.com>
2025-01-04 18:35:26 +0530

    streamlit basics added

---
+++ intro.py
@@ -0,0 +1,10 @@
+# pip install streamlit numpy pandas matplotlib
+# Run streamlit app with command "streamlit run pythonfilename.py"
+# streamlit supports hot reloading i.e, the changes made in the code are updated and we just have to refresh the streamlit app on our browser and the changes will be reflected
+# select the always rerun option on the streamlit app so that you don't even have to refresh to see the changes
+# So enable autosave in VS code and select always rerun option on the streamlit app and the changes made in the code will automatically reflect in the app
+
+import streamlit as st
+
+st.write("Hello Universe!")
+
